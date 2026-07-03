cb0949377bdda6062b0d089322efd29e053518ef
idrisamohammedjibril <idrisamohammedjibril@gmail.com>
2026-07-03 07:27:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-function showMessage(){
-    alert("Welcome to Shuwa First Website!");
+function showMessage() {
+  alert("Thank you for your interest in Shuwa Foundation. We will contact you soon!");
 }
